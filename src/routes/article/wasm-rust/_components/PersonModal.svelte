449f8f5752bf59821af8c14c7250de576257425f
monologue_mind <michaelb5797@gmail.com>
2022-11-11 00:32:56 +0000

    recovering files from old system

---
+++ src/routes/article/wasm-rust/_components/PersonModal.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Modal from '$components/Modal.svelte';
+</script>
+
+<Modal
+	modal_confirm_handler={() => console.log('Confirm')}
+	modal_cancel_handler={() => console.log('Cancel')}
+	modal_cancel_text="Cancel"
+	modal_confirm_text="Confirm"
+	modal_open_text="Create Person"
+	modal_title="Create Person"
+>
+	<h1 class="text-5xl font-bold">This is an h1</h1>
+</Modal>
